49aa27c7d2f7bf75b3dbdeae00d7e6092e7fcc80
Marco Valeri <marcovaleri@hotmail.it>
2021-12-15 18:54:21 +0000

    Create main page

---
+++ src/components/word-counter/word-counter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Import style file
+import './word-counter.scss';
+
+const WordCounter = () => {
+    return (
+        <h1>Word Counter</h1>
+    )
+}
+
+export default WordCounter;

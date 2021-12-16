4f7d577161fce4088f387246a638be0e34bc79ab
Marco Valeri <marcovaleri@hotmail.it>
2021-12-16 18:28:26 +0000

    Add logic that allow to count the characters

--- src/components/word-counter/word-counter.js
+++ src/components/word-counter/word-counter.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Import style file
 import './word-counter.scss';
 
 const WordCounter = () => {
+
+    // Set states
+    const [ textInput, setTextInput ] = useState("");
+
+    // Events
+    const userInputHandler = e => {
+        setTextInput(e.target.value);
+    }
+
     return (
-        <h1>Word Counter</h1>
+        <form>
+            <textarea value={textInput} onChange={userInputHandler} cols="50" rows="5"></textarea>
+            <section>
+                <h2>Output:</h2>
+                <p>Characters: {textInput.replaceAll('\n', '').length}</p>
+                <p>Text: {textInput}</p>
+            </section>
+        </form>
     )
+
 }
 
 export default WordCounter;
